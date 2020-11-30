3fd91ef2628e0499759180122a61533f8700917e
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2020-11-30 17:21:41 +0200

    first commit

---
+++ Oath2Sso/src/test/java/com/Oath2Sso/demo/Oath2SsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Oath2Sso.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oath2SsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
